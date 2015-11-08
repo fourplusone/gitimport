6ce30d95434f3f33f4aaa8e0478065d824850a3b
Matthias Bartelmeß <mba@fourplusone.de>
2015-11-08 16:40:18 +0100

    added author, install_requires

--- setup.py
+++ setup.py
@@ -3,8 +3,9 @@ from setuptools import setup, Extension
 setup(
     name='gitimport',
     description='Import python packages directly from a git repository',
+    author='Matthias Bartelmeß',
     author_email='mba@fourplusone.de',
     py_modules=['gitimport'],
     version='1.0.0',
-    requires=['pygit2'],
+    install_requires=['pygit2'],
 )
