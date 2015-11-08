46e4a2cfa871c97372e545fbaa441867faa7f9aa
Matthias Bartelmeß <mba@fourplusone.de>
2015-11-08 18:34:11 +0100

    added further pypi information

--- setup.py
+++ setup.py
@@ -3,9 +3,17 @@ from setuptools import setup, Extension
 setup(
     name='gitimport',
     description='Import python packages directly from a git repository',
+    long_description='',
     author='Matthias Bartelmeß',
     author_email='mba@fourplusone.de',
+    url='https://github.com/fourplusone/gitimport',
     py_modules=['gitimport'],
     version='1.0.0',
     install_requires=['pygit2'],
+    license='MIT',
+    classifiers=['Development Status :: 4 - Beta', 
+                 'License :: OSI Approved :: MIT License',
+                 'Programming Language :: Python :: 3.4',
+                 'Programming Language :: Python :: 3.5',
+                 'Programming Language :: Python :: 3 :: Only']
 )
